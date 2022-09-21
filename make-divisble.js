4472e50c330b456db857b2c5192d4c8e5d057e15
TreySchneider95 <tschneid95@gmail.com>
2022-09-21 13:32:42 -0600

    added check to make divisble

--- make-divisble.js
+++ make-divisble.js
@@ -2,9 +2,12 @@ const prompt = require('prompt-sync')({sigint: true});
 
 let x = prompt("enter first number: ")
 let y = prompt("enter second number: ")
-
-while (x%y !== 0){
-    console.log(x)
-    x ++
+if(y !== 0){
+    while (x%y !== 0){
+        console.log(x)
+        x ++
+    }
+    console.log(`${x} is divisible by ${y}`)
+}else{
+    console.log("Can't divide by 0")
 }
-console.log(`${x} is divisible by ${y}`)
